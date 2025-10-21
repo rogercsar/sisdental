63f833f2db70fd702dc48960886e36c90de2ab2d
rogercsar <rogercsardev@gmail.com>
2025-10-21 19:02:00 -0400

    correcoes build layout commit

--- frontend/src/pages/ImportarDados.tsx
+++ frontend/src/pages/ImportarDados.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { getSupabase } from '../lib/supabase';
 import Papa from 'papaparse';
 import * as XLSX from 'xlsx';
